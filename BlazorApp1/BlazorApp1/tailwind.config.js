f0dc1a71bdaaacf195a908dcd0f336e8c060f7f1
pavowax <dincer.guner1234@gmail.com>
2024-12-19 13:48:51 +0300

    First Commit

---
+++ BlazorApp1/BlazorApp1/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./Pages/**/*.{razor,html}",
+        "./Components/**/*.{razor,html}",
+        "./wwwroot/**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
